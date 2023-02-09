2f4a345e7e20fe8962c2ef6897010b9c83dea15e
Nikos Nikiforos <115108703+nNikiforos@users.noreply.github.com>
2023-02-09 22:03:19 +0200

    activities.html finished content
    Finished the content from activities.html and made some changes

--- JS/activities.js
+++ JS/activities.js
@@ -1,9 +1,11 @@
 function changeTab(tabId) {
   const activeTabClassName = "activeTab";
+  const activeImgClassName = "activeImg";
 
   const clickedTab = document.getElementById(tabId);
   const activeTab = document.getElementsByClassName(activeTabClassName);
 
+  // tabId is checked for a href go to the specific tab
   const btn = document.getElementById(tabId + "-btn");
   btn.checked = true;
   console.log(btn);
@@ -11,9 +13,21 @@ function changeTab(tabId) {
   for (const tab of activeTab) {
     tab.classList.remove(activeTabClassName);
   }
-
   clickedTab.classList.add(activeTabClassName);
+
+  // Remove the activeImg class from all images
+  const images = document.querySelectorAll("img");
+  for (const image of images) {
+    image.classList.remove(activeImgClassName);
+  }
+
+  // Add the activeImg class to the image of the clicked tab
+  const imageId = tabId.replace("tab-", "");
+  const activeImage = document.getElementById(imageId);
+  activeImage.classList.add(activeImgClassName);
 }
+
+// location for tabId's for the a href
 if (location.hash) {
   let hashId = location.hash.replace("#", "");
   console.log(hashId);
@@ -25,6 +39,7 @@ if (location.hash) {
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
+const bodyS = document.getElementById("containerId");
 
 openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -51,10 +66,12 @@ function openModal(modal) {
   if (modal == null) return;
   modal.classList.add("active");
   overlay.classList.add("active");
+  bodyS.classList.add("dontScroll");
 }
 
 function closeModal(modal) {
   if (modal == null) return;
   modal.classList.remove("active");
   overlay.classList.remove("active");
+  bodyS.classList.remove("dontScroll");
 }
