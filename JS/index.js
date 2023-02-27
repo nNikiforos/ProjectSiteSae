2c1b265895d674aba5409f326efd35bc60502416
Nikos Nikiforos <115108703+nNikiforos@users.noreply.github.com>
2023-02-27 19:42:19 +0200

    svg icons and fovicon
    added 3 svg icons at contact.html with animation and a fovicon

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,5 @@
 "use strict";
 
-// BACKGROUND IMAGE
-const background = document.getElementById("mainimg");
-window.addEventListener("scroll", function () {
-  background.style.backgroundSize = 140 - +window.pageYOffset / 50 + "%";
-});
-
 //UP COMING EVENTS
 let i = 1;
 let eventimgs = [
@@ -56,46 +50,3 @@ gridItems.forEach((item) => {
   item.style.gridRowStart = rowStart;
   item.style.gridRowEnd = `span ${rowEnd - rowStart + 1}`;
 });
-
-// json top location content 'use strict';
-
-// Make a new XMLHttpRequest object
-const req = new XMLHttpRequest();
-
-// Set the request parameters
-req.open("GET", "../DataJson/index.json");
-req.responseType = "json";
-
-// Add the event listener for state changes
-req.addEventListener("readystatechange", () => {
-  // When the request is done...
-  if (req.readyState === XMLHttpRequest.DONE) {
-    // XMLHttpRequest.DONE === 4
-    // Check the response status and act accordingly
-    switch (req.status) {
-      case 200:
-        const swiperSlides = document.querySelectorAll(".swiper-slide");
-        // Remove existing slides, if any
-
-        // Show the slide data (from json) in a list
-        for (let i = 0; i < swiperSlides.length; i++) {
-          const content = req.response[i];
-          const h2 = document.createElement("h2");
-          const p = document.createElement("p");
-          p.textContent = `${content.text}`;
-          p.classList.add("info");
-          h2.textContent = `${content.title}`;
-          h2.classList.add("location");
-
-          swiperSlides[i].appendChild(h2);
-          swiperSlides[i].appendChild(p);
-        }
-        break;
-      case 404:
-        console.error("Not found");
-        break;
-    }
-  }
-});
-
-req.send();
